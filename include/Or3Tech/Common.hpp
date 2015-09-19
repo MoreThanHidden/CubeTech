56efe16fbde4967146494ae9e16838c52f030a9c
MoreThanHidden (John816) <john816_@hotmail.com>
2015-09-19 17:10:28 +0800

    Or3Tech
    Learning C++ by Making a Game

---
+++ include/Or3Tech/Common.hpp
@@ -0,0 +1,9 @@
+#ifndef OR3TECH_COMMON_HPP
+#define OR3TECH_COMMON_HPP
+
+namespace Or3Tech {
+	using Float32 = float;
+	using Float64 = double;
+}//namesapce Or3Tech
+
+#endif
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <Or3Tech/Common.hpp>
+
+int main(int argc, char** argv) {
+	std::cout << "Hello World!!\n";
+	std::cin.ignore();
+
+	return 0;
+}
