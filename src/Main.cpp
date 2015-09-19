f0bf2dbcd401cfd1dc686a15d436ed8dbb99b57d
MoreThanHidden (John816) <john816_@hotmail.com>
2015-09-20 03:08:39 +0800

    Included Glew and Shaders

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,54 +1,116 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <Or3Tech/Common.hpp>
+#include <Or3Tech/ShaderProgram.hpp>
 
-GLOBAL int g_windowWidth = 854;
-GLOBAL int g_windowHight = 480;
+GLOBAL const int g_windowWidth = 854;
+GLOBAL const int g_windowHeight = 480;
 
-int main(int argc, char** argv) {
-	
+INTERNAL void glfwHints()
+{
+	glfwWindowHint(GLFW_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_VERSION_MINOR, 1);
+}
+
+int main(int argc, char** argv)
+{
 	GLFWwindow* window;
 
 	if (!glfwInit())
 		return EXIT_FAILURE;
 
-	window = glfwCreateWindow(g_windowWidth, g_windowHight, "Or3Tech", nullptr, nullptr);
-		if (!window) {
 
-			glfwTerminate();
-			return EXIT_FAILURE;
+	glfwHints();
+	window = glfwCreateWindow(g_windowWidth, g_windowHeight, "Or3Tech", nullptr, nullptr);
 
+	if (!window)
+	{
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewInit();
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	float vertices[] = {
+		// x,    y,    r,   g,    b
+		+0.5f, +0.5f, 1.0f, 1.0f, 1.0f,// Vertex 0
+		-0.5f, +0.5f, 0.0f, 0.0f, 1.0f,// Vertex 1
+		+0.5f, -0.5f, 0.0f, 1.0f, 0.0f,// Vertex 2
+		-0.5f, -0.5f, 1.0f, 0.0f, 0.0f,// Vertex 3
+	};
+
+
+	GLuint vbo; // Vertex Buffer Object
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	Or3Tech::ShaderProgram shaderProgram;
+	shaderProgram.attachShaderFromFile(Or3Tech::ShaderType::Vertex, "data/shaders/default.vert.glsl");
+	shaderProgram.attachShaderFromFile(Or3Tech::ShaderType::Fragment, "data/shaders/default.frag.glsl");
+
+	shaderProgram.bindAttribLocation(0, "vertPosition");
+	shaderProgram.bindAttribLocation(1, "vertColor");
+	shaderProgram.link();
+	shaderProgram.use();
+
+
+	bool running = true;
+	bool fullscreen = false;
+	while (running){
+		
+
+		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);{
+
+			glEnableVertexAttribArray(0);
+			glEnableVertexAttribArray(1);
+
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (const GLvoid*)(2 * sizeof(float)));
+
+			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+			glDisableVertexAttribArray(0);
+			glDisableVertexAttribArray(1);
 		}
 
-		glfwMakeContextCurrent(window);
 
-		bool running = true;
-		bool fullscreen = false;
-		while (running) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 
-			glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT);
+		if (glfwWindowShouldClose(window) ||
+			glfwGetKey(window, GLFW_KEY_ESCAPE))
+			running = false;
 
-			glfwSwapBuffers(window);
-			glfwPollEvents();
+		if (glfwGetKey(window, GLFW_KEY_F11)){
+			fullscreen = !fullscreen;
 
-			if (glfwWindowShouldClose(window) || glfwGetKey(window, GLFW_KEY_ESCAPE))
-				running = false;
+			GLFWwindow* newWindow;
 
-			if (glfwGetKey(window, GLFW_KEY_F11)){
-				fullscreen = !fullscreen;
-				GLFWwindow* newWindow;
+			glfwHints();
+			if (fullscreen){
 
-				if (fullscreen) {
-
-					int count;
-					const GLFWvidmode* modes = glfwGetVideoModes(glfwGetPrimaryMonitor(), &count);
-
+				int count;
+				const GLFWvidmode* modes =
+					glfwGetVideoModes(glfwGetPrimaryMonitor(), &count);
 					newWindow = glfwCreateWindow(modes[count - 1].width, modes[count - 1].height, "Or3Tech", glfwGetPrimaryMonitor(), window);
+
 				}else{
-					newWindow = glfwCreateWindow(g_windowWidth, g_windowHight, "Or3Tech", nullptr, window);
+
+					newWindow = glfwCreateWindow(g_windowWidth, g_windowHeight, "Or3Tech", nullptr, window);
+
 				}
 				
 				glfwDestroyWindow(window);
@@ -56,6 +118,7 @@ int main(int argc, char** argv) {
 				glfwMakeContextCurrent(window);
 
 			}
+
 		}
 
 		glfwTerminate();
