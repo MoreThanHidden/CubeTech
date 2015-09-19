f31d9004134f0cb19865d9d612c9430df26e3a4f
MoreThanHidden (John816) <john816_@hotmail.com>
2015-09-19 20:08:06 +0800

    GLFW3 setup
    Stating to setup using GLFW 3.1.1

--- include/Or3Tech/Common.hpp
+++ include/Or3Tech/Common.hpp
@@ -2,8 +2,14 @@
 #define OR3TECH_COMMON_HPP
 
 namespace Or3Tech {
+
 	using Float32 = float;
 	using Float64 = double;
+
+#define GLOBAL static;
+#define INTERNAL static;
+#define LOCAL_PERSIST static;
+
 }//namesapce Or3Tech
 
 #endif
